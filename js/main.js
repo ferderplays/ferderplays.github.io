2863c1994f3ea80c28b59e757835d28a0f5c9c08
ferderplays <86313430+ferderplays@users.noreply.github.com>
2024-05-15 19:35:28 +0200

    rewrite

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    $(".age").text("15")
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.countapi.xyz/hit/website.ferder.repl.co/visits");
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        $("#visits").text(this.response.value);
+    }
+    xhr.send();
+    $(window).scroll(() => {
+        if ($(window).scrollTop()) {
+            $("header").addClass("fixed");
+        } else {
+            $("header").removeClass("fixed");
+        }
+    })
+})
